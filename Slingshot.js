b60922bb35281e13b1ca42341fcc372d215c4283
RachitMundhara <77496900+RachitMundhara@users.noreply.github.com>
2021-05-25 19:58:19 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,19 @@
+class Slingshot{
+
+    constructor(body1,body2){
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:0.04,
+            length:10,
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain)
+    }
+
+    display(){
+        var pointA = this.chain.bodyA;
+        var pointB = this.chain.bodyB;
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
